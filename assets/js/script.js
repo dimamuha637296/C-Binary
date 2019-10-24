bdc1bac118e6f602844147ec3457428933d35a40
Dmitriy <dima.muha637296@gmail.com>
2019-10-24 17:14:54 +0300

    Залив GetMargin

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-var ready = function(){
-    setTimeout(function(){
-        var el = document.querySelector("body")
-        el.classList.add("active");
-    },10);
-}
-document.addEventListener("DOMContentLoaded", ready);
